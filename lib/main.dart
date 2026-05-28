f74b99e185ecaa7d05046a131d8a7bbd6dfc19ac
Saroj9344 <sarojtembhare10@gmail.com>
2026-05-28 21:19:26 +0530

    error resolved

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
